0d61d0d2fd7f91819e7d9d36d838db7e4daed87e
TheMadLord-study <terranovas121@gmail.com>
2021-06-22 00:10:22 +0300

    scss as module

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.scss';
+import st from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={st.app}>
+      <header className={st.header}>
+
+
+
       </header>
     </div>
   );
